85d84a093afd6917ccd05e2f2a1270ca21a39894
MiltonDM <36650054+MiltonDM@users.noreply.github.com>
2018-05-23 20:19:11 -0500

    Update ServoB.ino

--- ProgramaArduino/ServoB/ServoB.ino
+++ ProgramaArduino/ServoB/ServoB.ino
@@ -1,81 +1,103 @@
-#include <Servo.h>  //libreria para manejar los servomotores del brazo
+#include <Servo.h>
+#include <EEPROM.h>
 #include  <LiquidCrystal.h>  //libreria para trabajar con la pantalla LCD
 
+Servo motor1;  //se declara motor a utilizar
+Servo motor2;  //se declara motor a utilizar
+Servo motor3;  //se declara motor a utilizar
 
-Servo motor1;   //se declara el servomotor numero 1
-Servo motor2;   //se declara el servomotor numero 2
-Servo motor3;   //se declara el servomotor numero 1
-int num;        //varibale que recibe los parametros que se mandan desde java y el cual contribuira
-int mov;
+int num;    //variable que obtendra el valor enviado de java
+int mov;    //variable que regulara el movimiento de lso servomotres 
 int estado = 0; //Guarda el estado del boton
 int salida = 0; //0= led esta apagado 1 encendido
 int estadoAnterior = 0; //estado anterior del boton
 //int pinzumbador = 13;    // pin del zumbador
 int frecuencia = 220;      // frecuencia correspondiente a la nota La
-LiquidCrystal lcd (7, 6, 5, 4, 3, 2);
 
-void setup(){
+int pin_Button = 12;        //pin que se utiliza para el boton de reproducir
+int posiciones[300];        //el maximo de posiciones que se pueden guardar
+int pin_Button_State = 0;   //boton que ejecutara movimientos
+int pin_Button_State_Last = 0;  //boton a activar mediante pin_Button cuando este este en HIGH
+//int contador = 0;
+int datos = 0;            //contador que reproducira los valoras guardados
+
+
+void setup() {
   Serial.begin(28800);
-  //inicializamos la posicion inicial del servo de cada uno
-  //cada servo en sus respectivos pin
-  motor1.attach(11);
-  motor1.write(171);
+  motor1.attach(9);       //declaramos que pin utiizara el motor 1
+  motor1.write(0);        //en que posicion iniciara el motor 1
 
-  motor2.attach(12);
-  motor2.write(52);
+  motor2.attach(10);     //declaramos que pin utiizara el motor 2
+  motor2.write(52);      //en que posicion iniciara el motor 2
 
-  motor3.attach(13);
-  motor3.write(0);
-  
-  pinMode(8,INPUT);  //declaramos el boton como entrada
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(0,INPUT);
-  pinMode(1,INPUT);
+  motor3.attach(11);     //declaramos que pin utiizara el motor 3
+  motor3.write(171);     //en que posicion iniciara el motor 3
 
-     
+  pinMode(pin_Button, INPUT);  //declaramos el boton de entrada
+  pinMode(6,INPUT);  //declaramos el boton como entrada
+  pinMode(8, OUTPUT);//declaramos el led como salida.
+  pinMode(7, OUTPUT);//declaramos el led como salida
+
 }
 
-void loop(){
-  estado = digitalRead(8); //leer el estado del boton
+void loop() {
+estado = digitalRead(6); //leer el estado del boton
 
-  if(estado == HIGH && (estadoAnterior == LOW)){
+  if(estado == HIGH && (estadoAnterior == LOW)){ //comprueba el estadom del boton para guardar el boton de paro
     salida = 1 - salida;
   }
   estadoAnterior = estado; //guarda el valor actual
   
-  if(salida == 0){
-    digitalWrite(10, LOW);
-    digitalWrite(9,HIGH);
-    lcd.begin(16, 2); //Nos indica donde empezara a excribir el LCD.    
-  lcd.print("Brazo Trabajando");
+  if(salida == 0){//si es estado es cero osea el boton sin preddsionar enciende el led
+    digitalWrite(7, LOW);
+    digitalWrite(8,HIGH);
     
-  if(Serial.available()>0){//incializamos el serial
-    num = Serial.read();//leemos el serial para la comunicacion
-    if(num >=0 && num <85){//valores de los servos dependiendo del programa que mande
-      mov = map(num, 0, 84, 0, 180);
-      motor1.write(mov);
+  
+  if (Serial.available() > 0) {    //comprobamos si es mayor a cero, si lo es ejecutara los movimientos y guardara
+    num = Serial.read();          //lee el numero enviado desde java
+    if (num >= 0 && num < 85) {   //si esta entre estas posiciones se movera y guardara
+      mov = map(num, 0, 84, 0, 180);  //los movemos a una variable, donde le pasamos num con vaor maxiomo de 180
+      motor1.write(mov);              //se mueve el motor con los parametros de mov
+      posiciones[datos] = mov;        //guardamos los datos para despues reproducirlos
+      datos++;                        //aumentamos las posiciones para guardar
     }
-    if(num >=85 && num <170){//valores de los servos dependiendo del programa que mande
-      mov = map(num, 85, 169, 0, 180);
-      motor2.write(mov);
+    if (num >= 85 && num < 170) {    //lee el numero enviado desde java
+      mov = map(num, 85, 169, 0, 180);  //si esta entre estas posiciones se movera y guardara
+      motor2.write(mov);                //se mueve el motor con los parametros de mov
+      posiciones[datos] = mov;          //guardamos los datos para despues reproducirlos
+      datos++;                          //aumentamos las posiciones para guardar
     }
-    if(num >=170 && num <= 255){//valores de los servos dependiendo del programa que mande
-      mov = map(num, 170, 255, 0, 180);
-      motor3.write(mov);
+    if (num >=170 && num <= 255) {    //lee el numero enviado desde java
+      mov = map(num, 170, 255, 0, 180);   //si esta entre estas posiciones se movera y guardara
+      motor3.write(mov);                  //se mueve el motor con los parametros de mov
+      posiciones[datos] = mov;            //guardamos los datos para despues reproducirlos
+      datos++;                            //aumentamos las posiciones para guardar
     }
   }
-  }if(salida == 1){
-      digitalWrite(10,HIGH);
-    digitalWrite(9, LOW);
+  //
+pin_Button_State = digitalRead(pin_Button_State);  //lee el estado
+if(pin_Button_State != pin_Button_State_Last){ //si es diferente pin_Button_State que se obtuvo pin_Button_State con pin_Button_State_Last se ejecutara el codigo
+if(pin_Button_State == HIGH) {        //si esta en HIGH se ejecuta
+  int i;                              //se incializa una variable para recorrer el arreglo
+  for(i = 0; i < 300; i = i +1){    //se declara un for donde para recorrer el arreglo de posiciones guardadas
+    Serial.println(posiciones[i]);         //se lle por serial las posiciones 
+    delay(100);                            //le damos un retraso de 100 para que no haga movimientos brusos
+    motor1.write(posiciones[i]);          //activa los pasos de motor 1 que se hicieron desde java
+    delay(100);                           //le damos un retraso de 100 para que no haga movimientos brusos
+    motor2.write(posiciones[i]);          //activa los pasos de motor 2 que se hicieron desde java
+    delay(100);                           //le damos un retraso de 100 para que no haga movimientos brusos
+    motor3.write(posiciones[i]);          //activa los pasos de motor 3 que se hicieron desde java
+     } 
+    }
+  }
+  }if(salida == 1){ //si es presionado con el valor 1 
+      digitalWrite(7,HIGH);
+    digitalWrite(8, LOW);
 
-    lcd.begin(16, 2); //Nos indica donde empezara a excribir el LCD.    
-  lcd.print("Brazo Detenido");
 
    // tone(pinzumbador,frecuencia);    // inicia el zumbido
     //delay(1500);                    
     //noTone(pinzumbador);               // lo detiene a los dos segundos
     //delay(1000);
   }
-  
-} 
+}
